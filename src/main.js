f28782f0466747790d5787c0643528bf69799a51
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-09-07 15:14:03 -0400

    Update scaffold using pixiJS

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 import './style.css'
+const PIXI = require('pixi.js');
+
+// Create a PixiJS application
+const app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: 0x1099bb,
+});
+
+// Add the PixiJS canvas to the game-container div
+document.getElementById('game-container').appendChild(app.view);
+
+// Your game logic here...
+
+// Load and display your game assets (textures, sprites, etc.)
+
+// Handle player interactions and movement
+
+// Show information about you when the player enters different areas
+
+// Example: When clicking on the "Contact" area, show contact info
+document.getElementById('contact-info').addEventListener('click', () => {
+    // Display contact information or trigger game logic
+    alert('Contact information goes here.');
+});
+
+// Start the PixiJS application
+app.start();
