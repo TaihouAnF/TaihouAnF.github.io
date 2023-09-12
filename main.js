bb7f0f31bba368f2b996b846daefec84c6ce3f28
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-09-12 14:37:30 -0400

    Refactor and add linkedin info

--- main.js
+++ main.js
@@ -11,12 +11,14 @@ document.body.appendChild(app.view);
 
 const character = new PIXI.Sprite(PIXI.Texture.from('taihou.png'));
 character.anchor.set(0.5);
+character.scale.x = 0.5;
+character.scale.y = 0.5;
 character.x = app.screen.width / 8;
 character.y = app.screen.height / 4;
 app.stage.addChild(character);
 
 // Define the character's speed
-const characterSpeed = 5;
+const characterSpeed = 10;
 
 // Create an object to store key states
 const keys = {};
@@ -49,34 +51,31 @@ const handleControl = () => {
     }
 }
 
-const targetGithub = { x: app.screen.width / 4, y: 3 * app.screen.height / 4 };
-
-const spriteSetup = (sprite, visible, targetLocation) => {
+const spriteSetup = (sprite, visible, targetLocation, scale) => {
     sprite.anchor.set(0.5);
     sprite.visible = visible;
+    sprite.scale.x = scale;
+    sprite.scale.y = scale;
     sprite.x = targetLocation.x;
     sprite.y = targetLocation.y;
     app.stage.addChild(sprite);
 };
 
+const targetGithub = { x: app.screen.width / 4, y: 3 * app.screen.height / 4 };
+
 const githubSpriteWhite = new PIXI.Sprite(PIXI.Texture.from('github-mark-white.png'));
-// githubSpriteWhite.anchor.set(0.5);
-// githubSpriteWhite.visible = false;
-// githubSpriteWhite.x = targetGithub.x;
-// githubSpriteWhite.y = targetGithub.y;
-// app.stage.addChild(githubSpriteWhite);
-spriteSetup(githubSpriteWhite, false, targetGithub);
+spriteSetup(githubSpriteWhite, false, targetGithub, 0.5);
 
 const githubSpriteDark = new PIXI.Sprite(PIXI.Texture.from('github-mark.png'));
-// githubSpriteDark.anchor.set(0.5);
-// githubSpriteDark.visible = true;
-// githubSpriteDark.x = targetGithub.x;
-// githubSpriteDark.y = targetGithub.y;
-// app.stage.addChild(githubSpriteDark);
-spriteSetup(githubSpriteDark, true, targetGithub);
+spriteSetup(githubSpriteDark, true, targetGithub, 0.5);
 
+const targetLinkedIn = { x: app.screen.width / 2, y: 3 * app.screen.height / 4 };
 
+const linkedInSpriteWhite = new PIXI.Sprite(PIXI.Texture.from('LinkedIn-White.png'));
+spriteSetup(linkedInSpriteWhite, false, targetLinkedIn, 0.5);
 
+const linkedInSpriteDark = new PIXI.Sprite(PIXI.Texture.from('LinkedIn-Dark.png'));
+spriteSetup(linkedInSpriteDark, true, targetLinkedIn, 0.5);
 
 
 const proximityThreshold = 100; // Adjust this value as needed
@@ -105,19 +104,17 @@ const updateClickable = (sprite1, sprite2, boolean, url) => {
     sprite2.visible = !boolean;
 }
 
-
-
 // Create a game loop to update character position
 app.ticker.add(() => {
     handleControl();
 
     // Check player's proximity to the target location
     const playerNearLocationGithub = isPlayerNearLocation(character.x, character.y, targetGithub.x, targetGithub.y);
-    // const playerNearLocationLinkedIn = isPlayerNearLocation(character.x, character.y, targetLocation.x, targetLocation.y);
+    const playerNearLocationLinkedIn = isPlayerNearLocation(character.x, character.y, targetLinkedIn.x, targetLinkedIn.y);
     // const playerNearLocationItchio = isPlayerNearLocation(character.x, character.y, targetLocation.x, targetLocation.y);
     // Show or hide clickable sprites accordingly
     updateClickable(githubSpriteWhite, githubSpriteDark, playerNearLocationGithub, 'https://github.com/TaihouAnF');
-    // updateClickable(githubClickableSprite, playerNearLocation);
+    updateClickable(linkedInSpriteWhite, linkedInSpriteDark, playerNearLocationLinkedIn, 'https://www.linkedin.com/in/anson-feng/');
     // updateClickable(githubClickableSprite, playerNearLocation);
 });
 
