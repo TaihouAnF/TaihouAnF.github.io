2db358c2ea834447245cc5a385654619c916731e
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-09-12 13:23:13 -0400

    Add github dark icons and change control handle

--- main.js
+++ main.js
@@ -2,20 +2,12 @@ import './style.css'
 import * as PIXI from 'pixi.js';
 
 const app = new PIXI.Application({
-    // resizeTo: window,
     width: window.innerWidth,
     height: window.innerHeight,
     backgroundColor: 0x191970,
-    // transparent: true,
 });
 
 document.body.appendChild(app.view);
-
-// const background = new PIXI.Sprite(PIXI.Texture.from('background.png'));
-// background.anchor.set(0.5);
-// background.x = app.screen.width / 2;
-// background.y = app.screen.height / 2;
-// app.stage.addChild(background);
 
 const character = new PIXI.Sprite(PIXI.Texture.from('taihou.png'));
 character.anchor.set(0.5);
@@ -29,12 +21,18 @@ githubgithubIconContainer.x = app.screen.width / 4;
 githubIconContainer.y = 3 * app.screen.height / 4;
 app.stage.addChild(githubIconContainer);
 
-const githubClickableSprite = new PIXI.Sprite(PIXI.Texture.from('github-mark-white.png'));
-githubClickableSprite.anchor.set(0.5);
-// githubClickableSprite.x = app.screen.width / 2 + 100;
-// githubClickableSprite.y = app.screen.height / 2 + 200;
+const githubSpriteWhite = new PIXI.Sprite(PIXI.Texture.from('github-mark-white.png'));
+
+githubSpriteWhite.anchor.set(0.5);
+githubSpriteWhite.visible = false;
 githubIconContainer.addChild(githubClickableSprite);
-githubClickableSprite.position.set(0, 0)
+githubSpriteWhite.position.set(0, 0)
+
+const githubSpriteDark = new PIXI.Sprite(PIXI.Texture.FROM('github-mark.png'));
+githubSpriteDark.anchor.set(0.5);
+githubSpriteDark.visible = true;
+githubIconContainer.addChild(githubSpriteDark);
+githubSpriteDark.position.set(0, 0);
 
 // Define the character's speed
 const characterSpeed = 5;
@@ -51,29 +49,7 @@ window.addEventListener('keyup', (e) => {
     keys[e.key] = false;
 });
 
-const proximityThreshold = 100; // Adjust this value as needed
-
-function isPlayerNearLocation(playerX, playerY, targetX, targetY) {
-    const distance = Math.sqrt(Math.pow(playerX - targetX, 2) + Math.pow(playerY - targetY, 2));
-    return distance < proximityThreshold;
-}
-
-const updateClickable = (sprite, boolean) => {
-    sprite.visible = boolean;
-    sprite.interactive = boolean;
-    sprite.buttonMode = boolean;
-    if (boolean) {
-        sprite.cursor = 'pointer';
-    }
-}
-
-githubClickableSprite.on('pointerdown', () => {
-    // Handle the click event here
-    window.location.href = 'https://github.com/TaihouAnF';
-});
-
-// Create a game loop to update character position
-app.ticker.add(() => {
+const handleControl = () => {
     if (keys['w'] || keys['ArrowUp']) {
         character.y -= characterSpeed;
     }
@@ -89,11 +65,60 @@ app.ticker.add(() => {
     if (keys['d'] || keys['ArrowRight']) {
         character.x += characterSpeed;
     }
+}
+
+const proximityThreshold = 100; // Adjust this value as needed
+
+function isPlayerNearLocation(playerX, playerY, targetX, targetY) {
+    const distance = Math.sqrt(Math.pow(playerX - targetX, 2) + Math.pow(playerY - targetY, 2));
+    return distance < proximityThreshold;
+}
+
+const updateClickable = (sprite1, sprite2, boolean) => {
+    sprite1.visible = boolean;
+    sprite1.interactive = boolean;
+    sprite1.buttonMode = boolean;
+    
+    if (boolean) {
+        sprite1.cursor = 'pointer';
+    }
+
+    sprite2.visible = !boolean;
+    sprite2.interactive = !boolean;
+    sprite2.buttonMode = !boolean;
+}
+
+githubSpriteWhite.on('pointerdown', () => {
+    // Handle the click event here
+    window.location.href = 'https://github.com/TaihouAnF';
+});
+
+// Create a game loop to update character position
+app.ticker.add(() => {
+    // if (keys['w'] || keys['ArrowUp']) {
+    //     character.y -= characterSpeed;
+    // }
+
+    // if (keys['s'] || keys['ArrowDown']) {
+    //     character.y += characterSpeed;
+    // }
+
+    // if (keys['a'] || keys['ArrowLeft']) {
+    //     character.x -= characterSpeed;
+    // }
+
+    // if (keys['d'] || keys['ArrowRight']) {
+    //     character.x += characterSpeed;
+    // }
+    handleControl();
 
     // Check player's proximity to the target location
-    const playerNearLocation = isPlayerNearLocation(character.x, character.y, targetLocation.x, targetLocation.y);
-
+    const playerNearLocationGithub = isPlayerNearLocation(character.x, character.y, githubgithubIconContainer.x, githubgithubIconContainer.y);
+    // const playerNearLocationLinkedIn = isPlayerNearLocation(character.x, character.y, targetLocation.x, targetLocation.y);
+    // const playerNearLocationItchio = isPlayerNearLocation(character.x, character.y, targetLocation.x, targetLocation.y);
     // Show or hide clickable sprites accordingly
-    updateClickable(githubClickableSprite, playerNearLocation);
+    updateClickable(githubSpriteWhite, githubSpriteDark, playerNearLocationGithub);
+    // updateClickable(githubClickableSprite, playerNearLocation);
+    // updateClickable(githubClickableSprite, playerNearLocation);
 });
 
